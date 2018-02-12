74ee73569635e978eb893c0370bac12ebcdadbd9
lisguo <lisa.guo@stonybrook.edu>
2018-02-12 14:49:11 -0500

    Added login page

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,16 @@
+from flask import render_template, redirect
 from app import app
+from flask import request
+from app.forms import LoginForm
 
-@app.route('/')
-@app.route('/index')
+@app.route('/ttt/', methods=['GET','POST'])
+def ttt():
+	form = LoginForm()
+	if form.validate_on_submit():
+			print("Logged in!")
+			return redirect('/play/')
+	return render_template('login.html', title='Sign In', form=form)
 
-def index():
-	return "Hello world!"
+@app.route('/play/')
+def play():
+	return "Play!"
